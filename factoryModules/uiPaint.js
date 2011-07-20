ce402c94321d4451f2953b9483587796e0111826
mschmulen <matt.schmulen@gmail.com>
2011-07-20 15:16:40 -0500

    paint module

---
+++ factoryModules/uiPaint.js
@@ -0,0 +1,34 @@
+//<module version="1.0">ti.paint</module>
+var uiPaint = (function() {
+  	
+	var API = { };
+  	
+  	API.factoryView = function(opts){
+  		
+  		Titanium.Painter = require('ti.paint');
+  		
+    	var topView = Ti.UI.createView({ backgroundColor: 'green', width:320, height:480});
+    
+	    var painter = Titanium.Painter.createView({
+	    	top:10,
+			left:10,
+			right:10,
+			height:350 });
+		
+		topView.add(painter);
+		
+	    return topView;
+	};
+  	
+  	API.factoryWindow = function(opts){ 
+     	var win = Ti.UI.createWindow({title:'uiPaint'}); 
+     	win.addChild( factoryView( options ) ); 
+     	return win; 
+  	};
+  	
+  	return API;
+})(); //end uiPaint
+Ti.UI.currentWindow.add( uiPaint.factoryView({}) );
+//uiPaint.factoryWindow({}).open({modal:true});
+//uiPaint.factoryWindow({}).open({fullscreen:true});
+  
