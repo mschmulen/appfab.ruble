e79f8446ae31638d880d4d8a09d1b302f71120a7
Matt Schmulen <matt.schmulen@gmail.com>
2014-02-18 17:53:07 -0500

    sl update

--- commands/cmd2_commands.rb
+++ commands/cmd2_commands.rb
@@ -4,6 +4,24 @@ require 'ruble'
 # COMMAND+2  MACROS that create top level navigation metaphor
 # ********************************************************
 
+command 'TabWindow' do |cmd|
+  cmd.key_binding = "Control+2"
+  cmd.key_binding.mac = "Command+2"
+  
+  cmd.output = :insert_as_snippet
+  cmd.input = :selection, :line
+  cmd.invoke do |context|
+    
+    className = STDIN.read
+    input = STDIN.read
+    
+    input << "var winA = new Window(L('products')); \n"
+    input << "var tabA = Ti.UI.createTab({ title: L('products'), icon: '/images/KS_nav_ui.png', window: winA }); \n"
+    input << "self.addTab(tabA); \n"
+    input << " \n"
+  end
+end
+
 command 'TabGroup' do |cmd|
   cmd.key_binding = "Control+2"
   cmd.key_binding.mac = "Command+2"
@@ -22,6 +40,7 @@ command 'TabGroup' do |cmd|
     input << "tabGroup.open(); \n"
   end
 end
+
 
 command 'Dashboard' do |cmd|
   cmd.key_binding = "Control+2"
